872c2e0ab7afff29e6d758cb0a9416943334eab9
Omar Wilson <omar.wilson3@comcast.net>
2016-11-14 23:01:46 -0800

    trivial change
    change string all to some

--- SaveTheKittens/ViewController.swift
+++ SaveTheKittens/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print(("hello all"))
+        print(("hello some"))
     }
 
     override func didReceiveMemoryWarning() {
